2a8a596fad86efdae058776b4212d6be7fbdcd29
Andrey Shpak <ashpak@xbsoftware.by>
2022-10-10 18:14:54 +0300

    [dev] necessary parentID

--- data/data.go
+++ data/data.go
@@ -6,7 +6,7 @@ type TaskProps struct {
 	Text           string     `json:"text"`
 	Checked        bool       `json:"checked"`
 	DueDate        *time.Time `json:"due_date,omitempty"`
-	ParentID       int        `json:"parent,omitempty" gorm:"column:parent"`
+	ParentID       int        `json:"parent" gorm:"column:parent"`
 	ProjectID      int        `json:"project,omitempty" gorm:"column:project"`
 
 	AssignedUsers    []User `gorm:"many2many:assigned_users" json:"-"`
